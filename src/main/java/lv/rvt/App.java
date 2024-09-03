8149697d4a7e39d1af05cdc7222f44e894aa4aa2
23DP1ALuck <166118306+23DP1ALuck@users.noreply.github.com>
2024-09-03 09:43:47 +0000

    first script

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,7 +7,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello world!");
+        String name = "Artjoms";
+        int age = 16;
+        boolean isOnline = true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(isOnline);
     }
 
 }

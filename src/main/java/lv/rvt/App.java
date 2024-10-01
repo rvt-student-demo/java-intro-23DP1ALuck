d8146d42e78eeba747b7e9901a91a2554851a44c
t0mmyq <a231660al@gmail.com>
2024-10-01 11:18:03 +0300

    From one to parameter

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,35 +7,11 @@ import java.util.Scanner;
 
 public class App{
     public static void main(String[] args) {
-        
-        Scanner scanner = new Scanner(System.in);
-        int sum = 0;
-        int count = 0;
-        int odd = 0;
-        int even = 0;
-        System.out.println("Give numbers: ");
-
-        while(true){
-        int number = Integer.valueOf(scanner.nextLine());
-        if (number == -1){
-            break;
+        printUntilNumber(5);
+    }
+    public static void printUntilNumber(int number){
+        for(int i = 1; i<=number; i++){
+            System.out.println(i);
         }
-        if(number % 2 == 0){
-            even++;
-        }
-        else{
-            odd++;
-        }
-        sum += number;
-        count++;
-        }
-
-        System.out.println("Thx! Bye!");
-        System.out.println("Sum: " + sum);
-        System.out.println("Numbers: " + count);
-        System.out.println("Average: " + (double)sum/count);
-        System.out.println("Even: " + even);
-        System.out.println("Odd: " + odd);
-
     }
 }

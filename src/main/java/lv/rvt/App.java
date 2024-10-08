92bb310c8846d648bbb542c3b4d439df7cf7f369
t0mmyq <a231660al@gmail.com>
2024-10-08 09:48:24 +0300

    Star sign (4 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,15 +6,47 @@ import java.io.BufferedWriter;
 import java.util.Scanner;
 
 public class App{
-    public static void main(String[] args) {
-        divisibleByThreeInRange(2, 10);
-    }
 
-    public static void divisibleByThreeInRange(int beginning, int end){
-        for(int x = beginning; x <= end; x++){
-            if(x % 3 == 0){
-                System.out.println(x);
-            }
+    public static void printStars(int number){
+        
+        for(int i = 0; i < number ; i++){
+            System.out.print("*");
+        }
+        System.out.println("");
+    }
+    public static void printSquare(int size){
+        for(int x = 0; x < size; x++){
+            printStars(size);
+        }
+        System.out.println("");
+    }
+    public static void printRectangle(int width, int height){
+        for(int stars = 0; stars < height; stars++){
+            printStars(width);
         }
     }
-}
+    public static void printTriangle(int size){
+        for (int z = 1; z <= size; z++){
+            printStars(z);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Printing stars");
+        printStars(5);
+        printStars(3);
+        printStars(9);
+        System.out.println("");
+        System.out.println("Printing a square");
+        printSquare(4);
+        System.out.println("");
+        System.out.println("Printing a rectangle");
+        printRectangle(17, 3);
+        System.out.println("");
+        System.out.println("Printing a triangle");
+        printTriangle(4);
+    }
+    
+}   
+

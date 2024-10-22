ffd9f8edb80787ecdf491a526a8aac9a3805af1b
t0mmyq <a231660al@gmail.com>
2024-10-22 10:47:45 +0300

    Print in range

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,41 +8,28 @@ import java.util.Scanner;
 
 
 public class App{
-
-    public static void main(String[] args) {
-
-        ArrayList<Integer> numbers = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
-
-        while(true){
-            int number = scanner.nextInt();
-
-            if (number == -1){
-                break;
-            }
-            else{
-                numbers.add(number);
-            }
-    }   
-    
-        while(true){
-            System.out.println("From where?");
-            int begin = scanner.nextInt();
-            System.out.println("To where?");
-            int finish = scanner.nextInt();
-
-            if (begin < 0 || finish > (numbers.size() - 1)){
-                System.out.println("Incorrect value");
-                continue;
-            } 
-            else{
-                for(int i = begin; i < (finish + 1); i++){
-                    System.out.println(numbers.get(i));
-                }
-            break;
+    public static void printNumbersInRange(ArrayList<Integer> numbers, int lowerLimit, int upperLimit){
+        for (int number : numbers){
+            if (number >= lowerLimit && number <= upperLimit){
+                System.out.println(number);
             }
         }
     }
-}    
-   
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(6);
+        numbers.add(-1);
+        numbers.add(5);
+        numbers.add(1);
+
+        System.out.println("The numbers in the range [0, 5]");
+        printNumbersInRange(numbers, 0, 5);
+
+        System.out.println("The numbers in the range [3, 10]");
+        printNumbersInRange(numbers, 3, 10);
+
+    }
+}
 

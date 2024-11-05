b29d0880a78d8d0b71d4f04293f85306e62d76af
t0mmyq <a231660al@gmail.com>
2024-11-05 09:49:08 +0200

    Decreasing counter (3 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -9,17 +9,46 @@ import java.util.Scanner;
 
 class App{
     public static void main(String[] args) {
+        DecreasingCounter counter = new DecreasingCounter(10);
+        DecreasingCounter counter2 = new DecreasingCounter(2);
+        DecreasingCounter counter3 = new DecreasingCounter(100);
 
-        Product banana = new Product("Banana", 1.1, 13);
-        banana.printProduct();
+        // part 1
+        System.out.println("Part 1");
 
-        // Person pers1 = new Person("Adrians");
+        counter.printValue();
 
-        // pers1.printPerson();
-        // pers1.growOlder();
-        // pers1.growOlder();
-        // pers1.growOlder();
-        // pers1.printPerson();
+        counter.decrement();
+        counter.printValue();
+
+        counter.decrement();
+        counter.printValue();
+
+
+        // part 2
+        System.out.println("Part 2");
+        counter2.printValue();
+
+        counter2.decrement();
+        counter2.printValue();
+
+        counter2.decrement();
+        counter2.printValue();
+
+        counter2.decrement();
+        counter2.printValue();
+
+        // part 3
+        System.out.println("Part 3");
+        counter3.printValue();
+
+        counter3.reset();
+        counter3.printValue();
+
+        counter3.decrement();
+        counter3.printValue();
+
+
    }    
 }
 
---
+++ src/main/java/lv/rvt/DecreasingCounter.java
@@ -0,0 +1,24 @@
+package lv.rvt;
+
+class DecreasingCounter{
+    private int value;
+
+    public DecreasingCounter(int initialValue){
+        this.value = initialValue;
+    }
+
+    public void printValue(){
+        System.out.println("value: " + this.value);
+    }
+
+    public void decrement(){
+        if(this.value > 0){
+            this.value--;
+        }
+    }
+
+    public void reset(){
+        this.value = 0;
+    }
+
+}

8500d470294494a11e358b3096eb045d3ffb556c
t0mmyq <a231660al@gmail.com>
2024-11-12 10:23:47 +0200

    File reader

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,73 +8,15 @@ import java.util.Scanner;
 
 
 class App{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+        BufferedReader reader = Utils.getReader("data.csv");
 
-        System.out.println("Part 1,2");
+        String line;
 
-        PaymentCard card = new PaymentCard(50);
-        System.out.println(card);
-
-        card.eatAffordably();
-        System.out.println(card);
-
-        card.eatHeartily();
-        card.eatAffordably();
-        System.out.println(card);
-
-        System.out.println("Part 3");
-        PaymentCard card33 = new PaymentCard(5);
-        System.out.println(card33);
-
-        card33.eatHeartily();
-        System.out.println(card33);
-
-        card33.eatHeartily();
-        System.out.println(card33);
-
-        System.out.println("Part 4");
-
-        PaymentCard card2 = new PaymentCard(10);
-        System.out.println(card2);
-
-        card2.addMoney(15);
-        System.out.println(card2);
-
-        card2.addMoney(10);
-        System.out.println(card2);
-
-        card2.addMoney(200);
-        System.out.println(card2);
-
-        System.out.println("Part 5");
-        PaymentCard card3 = new PaymentCard(10);
-        System.out.println("Paul: " + card3);
-        card.addMoney(-15);
-        System.out.println("Paul: " + card3);
-
-        System.out.println("Part 6");
-
-        PaymentCard paulsCard = new PaymentCard(20);
-        PaymentCard mattsCard = new PaymentCard(30);
-
-        paulsCard.eatHeartily();
-        mattsCard.eatAffordably();
+        while((line = reader.readLine()) != null){
+            System.out.println(line);
+        }
         
-        System.out.println("Paul: " + paulsCard);
-        System.out.println("Matt: " + mattsCard);
-
-        paulsCard.addMoney(20);
-        mattsCard.eatHeartily();
-
-        System.out.println("Paul: " + paulsCard);
-        System.out.println("Matt: " + mattsCard);
-
-        paulsCard.eatAffordably();
-        paulsCard.eatAffordably();
-        mattsCard.addMoney(50);
-
-        System.out.println("Paul: " + paulsCard);
-        System.out.println("Matt: " + mattsCard);
     }    
 }
 

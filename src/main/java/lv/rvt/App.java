af13bdc4f7bdcfaf1278304325e7537653b5deba
t0mmyq <a231660al@gmail.com>
2024-12-10 12:21:29 +0200

    Health station (3 parts - 3)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -17,17 +17,20 @@ public class App {
 
         Person ethan = new Person("Ethan", 1, 7, 110);
         Person peter = new Person("Peter", 33, 85, 176);
-    
-        System.out.println(ethan.getName() + " weight: " + childrensHospital.weigh(ethan) + " kilos");
-        System.out.println(peter.getName() + " weight: " + childrensHospital.weigh(peter) + " kilos");
-    
-        childrensHospital.feed(ethan);
-        childrensHospital.feed(ethan);
-        childrensHospital.feed(ethan);
-    
-        System.out.println("");
-    
-        System.out.println(ethan.getName() + " weight: " + childrensHospital.weigh(ethan) + " kilos");
-        System.out.println(peter.getName() + " weight: " + childrensHospital.weigh(peter) + " kilos");
+
+
+        System.out.println("weighings performed: " + childrensHospital.weighings());
+
+        childrensHospital.weigh(ethan);
+        childrensHospital.weigh(peter);
+
+        System.out.println("weighings performed: " + childrensHospital.weighings());
+
+        childrensHospital.weigh(ethan);
+        childrensHospital.weigh(ethan);
+        childrensHospital.weigh(ethan);
+        childrensHospital.weigh(ethan);
+
+        System.out.println("weighings performed: " + childrensHospital.weighings());
     }
 }
--- src/main/java/lv/rvt/HealthStation.java
+++ src/main/java/lv/rvt/HealthStation.java
@@ -1,10 +1,15 @@
 package lv.rvt;
 
 public class HealthStation {
+    private int weighningsCounter;
     public double weigh(Person person){
+        weighningsCounter++;
         return person.getWeight();
     }
     public void feed(Person person){
         person.setWeight(person.getWeight() + 1);
+    }
+    public int weighings(){
+        return weighningsCounter;
     }
 }

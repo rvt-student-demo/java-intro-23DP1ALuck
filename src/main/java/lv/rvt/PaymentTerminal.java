c2b742c0748b14b9ddd28d52d6e36734c610f6a8
t0mmyq <a231660al@gmail.com>
2024-12-11 19:52:48 +0200

    Payment card (4 parts - 3)

--- src/main/java/lv/rvt/PaymentTerminal.java
+++ src/main/java/lv/rvt/PaymentTerminal.java
@@ -29,6 +29,22 @@ public class PaymentTerminal {
         }
     }
 
+    public boolean eatAffordably(PaymentCard card){
+        if(card.getBalance() >= 2.5){
+            card.takeMoney(2.50);
+            this.affordableMeals += 1;
+            return true;
+        }return false;
+    }
+
+    public boolean eatHeartily(PaymentCard card){
+        if(card.getBalance() >= 4.3){
+            card.takeMoney(4.3);
+            this.heartyMeals += 1;
+            return true;
+        }return false;
+    }
+
     public String toString() {
         return "money: " + money + ", number of sold afforable meals: " + affordableMeals + ", number of sold hearty meals: " + heartyMeals;
     }
